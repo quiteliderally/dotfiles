330d75ef7248d9d4d4ec7b18af80423efef645b3
root <root@q1.(none)>
2011-09-07 09:05:20 -0500

    added an installer

---
+++ install.sh
@@ -0,0 +1,10 @@
+#! /bin/sh
+
+rm ~/.bashrc
+ln -s ~/.dotfiles/bashrc ~/.bashrc
+
+ln -s ~/.dotfiles/vimrc ~/.vimrc
+ln -s ~/.dotfiles/vim ~/.vim
+
+mkdir ~./ssh
+cat ~/.dotfiles/ssh/authorized_keys >> ~/.ssh/authorized_keys
